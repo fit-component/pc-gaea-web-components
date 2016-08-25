ec2ecee854bfe8c9610affb3caaf36bc96949057
huangziyi01 <huangziyi01@baidu.com>
2016-08-25 10:41:55 +0800

    quick push

---
+++ readme.md
@@ -0,0 +1,9 @@
+# 盖亚基础组件
+
+传入编辑器或者 `Preview` 以拓展基础组件支持
+
+---
+
+````jsx
+npm install fit-gaea-web-components
+````
---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+/// <reference path="../../../../typings/tsd.d.ts" />
+/// <reference path="../../gaea/models/tsd.d.ts" />
+
+import * as React from 'react'
+import Button from './button'
+import Input from './input'
+import Layout from './layout'
+import Loading from './loading'
+import Select from './select'
+import Paragraph from './paragraph'
+
+export default [Layout, Paragraph, Button, Input, Select, Loading] as Array<React.ComponentClass<FitGaea.ComponentProps>>
